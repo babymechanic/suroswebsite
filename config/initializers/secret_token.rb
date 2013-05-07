4581afb9af3fec0378d66b16868bd89032db7888
piggy <babymechanic@gmail.com>
2013-05-07 23:13:53 +0530

    piggy: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suroswebsite::Application.config.secret_token = '26601caa63fd9f0856ffe798e0613eb9b8110f1fc088ac11701b950093845a5bdefc7b8792d7fb2797b1b67376908c21612696d1c152814f608d766222ea9513'
