9d6ed59d03fb4d409728b640e721b86cc236f181
piggy <babymechanic@gmail.com>
2013-05-08 18:19:56 +0530

    piggy: added pics of suro on the home page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController  < ActionController::Base
 
   def home
+    @about = Refinery::Page.where("slug = 'about'")[0]
+                           .parts
+                           .find{|x|x.title.downcase == 'body'}
+                           .body
     render :layout => 'application'
   end
 
