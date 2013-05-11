f91dbfa8f64211cdfc17afd45aa42fb796f6de38
piggy <babymechanic@gmail.com>
2013-05-11 19:52:29 +0530

    piggy: added layout to render the page of elements.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,24 @@
 class ProjectsController < ActionController::Base
 
   def index
+    @projects = projects_for 1
+    @current_page = 1
+    @total_pages = number_of_pages(Refinery::Projects::Project)
     render :layout => 'application'
   end
+
+  private
+
+      def projects_for(page_number)
+        Refinery::Projects::Project.offset((page_number - 1) * Constants::ITEMS_PER_PAGE)
+                                   .limit(Constants::ITEMS_PER_PAGE)
+                                   #.where("falls_under_#{pillar_name}_pillar")
+      end
+
+      def number_of_pages(active_record_class)
+        pages = (active_record_class.count / Constants::ITEMS_PER_PAGE)
+        pages > 0 ? pages.ceil : 0
+      end
+
 
 end
---
+++ app/models/constants.rb
@@ -0,0 +1,3 @@
+module Constants
+  ITEMS_PER_PAGE = 3
+end
